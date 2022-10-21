e4be6fbabb40e291254e2efde147aadb09ba1312
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-21 13:41:51 +0200

    setup mappings profile

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using dotnet6_api.Dtos.Character;
+
+namespace dotnet6_api
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
