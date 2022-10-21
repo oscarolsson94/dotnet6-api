c7f03af0d30cd697a085466e3635c241e77db77e
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-21 09:58:47 +0200

    created character service interface

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet6_api.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        
+    }
+}
---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet6_api.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+
+        Character GetCharacterById();
+
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
