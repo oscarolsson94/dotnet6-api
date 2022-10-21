7b0297e48fa9ec89b61d26dcf7392a3ec606f163
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-21 13:44:33 +0200

    generating new Id

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -24,8 +24,15 @@ namespace dotnet6_api.Services.CharacterService
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            characters.Add(_mapper.Map<Character>(newCharacter));
+
+            Character character = _mapper.Map<Character>(newCharacter);
+
+            character.Id = characters.Max(character => character.Id) + 1;
+
+            characters.Add(character);
+
             serviceResponse.Data = characters.Select(character => _mapper.Map<GetCharacterDto>(character)).ToList();
+            
             return serviceResponse;
         }
 
@@ -37,8 +44,11 @@ namespace dotnet6_api.Services.CharacterService
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
+
             var character = characters.FirstOrDefault(character => character.Id == id);
+
             serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+
             return serviceResponse;
         }
     }
